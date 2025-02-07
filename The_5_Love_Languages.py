5961b573e4e673dbfb7b54c5d5e61170c97516fd
Kukojaka <BKS22@yandex.ru>
2025-02-07 18:58:01 +0300

    Update The_5_Love_Languages.py

--- The_5_Love_Languages.py
+++ The_5_Love_Languages.py
@@ -1,6 +1,13 @@
 from preloaded import LOVE_LANGUAGES
 
 def love_language(partner, weeks):
-    x = 0
-    print(partner.response("words"))
+    
+    l = ['words', 'acts' , 'time', 'touch', 'gifts']
+    
+    for n in range (0, 49):
+        k = n%5
+        print(k)
+        
+        print(l[k])
+        print(partner.response(l[k]), weeks)
     return "words"
